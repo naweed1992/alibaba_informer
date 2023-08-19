8f43fdf4ebf668a4c220ea02a75c8bb88fb84263
william <william@domain.local>
2023-08-19 10:44:16 +0330

    add requirements.txt file and change port

--- main.py
+++ main.py
@@ -71,4 +71,4 @@ def kill_running_process(email):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=3240, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=1040, reload=True)
